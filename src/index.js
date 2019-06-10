d86390b21d354e50523b6498c323ac9c3baa22f7
Trevor Polischuk <trevor.polischuk@gmail.com>
2019-06-10 15:27:39 -0700

    Setting latitude using state

--- src/index.js
+++ src/index.js
@@ -5,17 +5,25 @@ class App extends React.Component {
 
   constructor(props) {
     super(props);
+
+    //This is the only time you can directly assign state
     this.state = { lat: null };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        // We called setstate!!
+        this.setState({ lat: position.coords.latitude});
+
+        //We did not try to change the value of the state directly
+        //this.state.lat = position.coords.latitude // This is bad/impossible!
+      },
+      (err) => console.log(err)
+    );
   }
 
   // React says we have to define render!!
   render() {
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
-      (err) => console.log(err)
-    );
-
-    return <div>Latitude: </div>
+    return <div>Latitude: {this.state.lat}</div>
   }
 }
 
