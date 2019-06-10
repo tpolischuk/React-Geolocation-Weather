3f6fd169e2f419029bd8249cae6d1d791c15292b
Trevor Polischuk <trevor.polischuk@gmail.com>
2019-06-09 23:19:10 -0700

    Switching to class based component and initializing null state for latitude

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { lat: null };
+  }
+
+  // React says we have to define render!!
   render() {
     window.navigator.geolocation.getCurrentPosition(
       (position) => console.log(position),
