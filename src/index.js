cf9c48e777bab8f03b70b237678da1e8366fc308
Trevor Polischuk <trevor.polischuk@gmail.com>
2019-07-09 16:27:23 -0700

    Ternary + config destructuring

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay'
 
 class App extends React.Component {
 
-  // constructor(props) {
-  //   super(props);
-  //   //This is the only time you can directly assign state
-  //   this.state = { lat: null, errorMessage: '' };
-  // }
-
+  // state without a constructor function
   // not this.state
   // so you can directly assign when you're initializing?
   state = { lat:null, errorMessage: ''}
@@ -26,7 +22,7 @@ class App extends React.Component {
       return <div>Error: {this.state.errorMessage}</div>
     }
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>
+      return <SeasonDisplay lat={this.state.lat}/>
     }
     return <div>Loading!</div>
   }
