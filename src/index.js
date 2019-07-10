39fede01c8a10ef9d7c58ecc838f8517bcbcd916
Trevor Polischuk <trevor.polischuk@gmail.com>
2019-07-10 11:48:40 -0700

    New spinner component for loading

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SeasonDisplay from './SeasonDisplay'
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 class App extends React.Component {
 
@@ -24,7 +25,7 @@ class App extends React.Component {
     if (!this.state.errorMessage && this.state.lat) {
       return <SeasonDisplay lat={this.state.lat}/>
     }
-    return <div>Loading!</div>
+    return <Spinner />;
   }
 }
 
