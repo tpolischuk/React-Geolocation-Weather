f7005f3b978b317bb7b46c0847b585ae0adcc6ac
Trevor Polischuk <trevor.polischuk@gmail.com>
2019-09-24 18:30:49 -0700

    Refactoring app class to a function, hooks

--- src/index.js
+++ src/index.js
@@ -1,40 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 import Spinner from './Spinner';
 
-class App extends React.Component {
 
-  // state without a constructor function
-  // not this.state
-  // so you can directly assign when you're initializing?
-  state = { lat:null, errorMessage: ''}
+const App = () => {
+  const [lat, setLat] = useState(null);
+  const [errorMessage, setErrorMessage] = useState('');
 
-  componentDidMount() {
+  useEffect(() => {
     window.navigator.geolocation.getCurrentPosition(
-      position => this.setState({ lat: position.coords.latitude}),
-      err => this.setState({ errorMessage: err.message })
+      position => setLat(position.coords.latitude),
+      err => setErrorMessage(err.message)
     );
+  }, []);
+
+  let content;
+  if (errorMessage) {
+    content = <div>Error: {errorMessage}</div>
+  } else if (lat) {
+    content = <SeasonDisplay lat={lat} />
+  } else {
+    content = <Spinner message="Please accept location request" />
   }
 
-  renderContent() {
-    if (this.state.errorMessage && !this.state.lat) {
-      return <div>Error: {this.state.errorMessage}</div>
-    }
-    if (!this.state.errorMessage && this.state.lat) {
-      return <SeasonDisplay lat={this.state.lat}/>
-    }
-    return <Spinner message="Please accept location request" />;
-  }
+  return <div className="border red">{content}</div>
 
-  render() {
-    return (
-      <div className="border red">
-        {this.renderContent()}
-      </div>
-    );
-  }
-}
+};
 
 ReactDOM.render(
   <App />,
